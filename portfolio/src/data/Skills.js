b10aa0b6f50244e76745c6ce4638793346b10644
joaosaraiva1999 <joaodnice@gmail.com>
2026-04-28 03:06:03 +0100

    Added a skills.js file to data and a Viewresume btn to the home page

---
+++ portfolio/src/data/Skills.js
@@ -0,0 +1,2 @@
+import { SiHtml5, SiJavascript, SiCss, SiReact, SiTailwindcss, SiBootstrap, SiWordpress, SiDotnet, SiMysql, SiPhp, SiLaravel, SiGit } from 'react-icons/si'
+import { Plug } from 'lucide-react'
--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import './Home.css'
 import { useContext } from 'react'
 import { ThemeContext } from '../../context/ThemeContext.jsx'
 import profile_pic from '../../assets/portfolio-pic.jpg'
+import { MdOutlineDownload } from 'react-icons/md'
 
 export default function Home() {
   const { theme, toggleTheme } = useContext(ThemeContext)
@@ -14,7 +15,7 @@ export default function Home() {
             <img src={profile_pic} alt="Profile Picture" />
           </div>
           <div className={`hero-work-status ${theme}`}>
-            <span className='dot'></span>
+            <span className={`dot ${theme}`}></span>
             <span className={`work-status-text ${theme}`}>AVAILABLE FOR WORK</span>
           </div>
           <div className="profile-name">
@@ -23,8 +24,14 @@ export default function Home() {
           <div className="profile-title">
             <p className={theme}>Junior Web Developer</p>
           </div>
+          <div className="profile-intro">
+            <p className={theme}>I'm a Junior Web Developer who enjoys building responsive, user-focused web applications. I work with JavaScript, React, HTML/CSS, ASP.NET, SQL and PHP/Laravel, and I like integrating APIs to connect systems and simplify workflows.</p>
+          </div>
+          <button className={`download-resume ${theme}`}>VIEW RESUME<span style={{ color: '#6B9EFF', paddingTop: '0.2rem' }}><MdOutlineDownload size={18} /></span></button>
         </section>
-        <div className={`hero_skills ${theme}`}></div>
+        <div className={`hero_skills ${theme}`}>
+
+        </div>
       </section>
     </main>
   )
