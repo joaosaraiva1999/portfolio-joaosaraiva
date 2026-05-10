80cf3a3fe8df28d5114393e1019f89ba04833a09
joaosaraiva1999 <joaodnice@gmail.com>
2026-05-11 00:43:17 +0100

    Created Objects for the About Me Page

---
+++ portfolio/src/data/AboutmeSkills.js
@@ -0,0 +1,32 @@
+
+
+export const AboutSkills = [
+  {
+    id: 1,
+    skill: 'Web Application Development',
+  },
+
+  {
+    id: 2,
+    skill: 'Front End Development',
+  },
+
+  {
+    id: 3,
+    skill: 'Back End Development',
+  },
+
+  {
+    id: 4,
+    skill: 'API Integration',
+  },
+
+  {
+    id: 5,
+    skill: 'Multimedia Integration for Web',
+  },
+
+  {
+    id: 6,
+    skill: 'Problem-Solving & Debugging',
+  }]
--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -1,10 +1,15 @@
 import './About.css'
+import { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext.jsx'
+import { skills } from '../../data/AboutmeSkills.js'
 
 export default function About() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
   return (
-    <main className="about-page">
-      <h2>About</h2>
-      <p>About page content goes here.</p>
+    <main className={`about-page ${theme}`}>
+      <section className={`about-left ${theme}`}></section>
+      <section className={`about-right ${theme}`}></section>
     </main>
   )
 }
